3f0411040b5885946168c063457cfe01820e6e70
rayen <rayenbenyoussef815@gmail.com>
2026-05-07 20:54:43 +0100

    operational and warehouse coding

--- sql/operational.sql
+++ sql/operational.sql
@@ -1,9 +1,23 @@
+
 create table insurance (
    age      number,
-   sex      varchar2(10),
+   gender      varchar2(10),
    bmi      number(5,2),
    children number,
    smoker   varchar2(5),
    region   varchar2(20),
    charges  number(10,2)
 )
+
+ORGANIZATION EXTERNAL (
+    TYPE ORACLE_LOADER
+    DEFAULT DIRECTORY insurance_dir
+    ACCESS PARAMETERS (
+        RECORDS DELIMITED BY NEWLINE
+        SKIP 1
+        FIELDS TERMINATED BY ','
+        OPTIONALLY ENCLOSED BY '"'
+    )
+    LOCATION ('insurance.csv')
+)
+REJECT LIMIT UNLIMITED;
