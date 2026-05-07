69d405c1e98700d1812c7d830aa98c4ea8068a5d
rayen <rayenbenyoussef815@gmail.com>
2026-05-07 15:51:14 +0100

    adding the docs

---
+++ sql/operational.sql
@@ -0,0 +1,9 @@
+create table insurance (
+   age      number,
+   sex      varchar2(10),
+   bmi      number(5,2),
+   children number,
+   smoker   varchar2(5),
+   region   varchar2(20),
+   charges  number(10,2)
+)
